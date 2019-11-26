46adcea1ccb767567196271be0b9a01cba3d02dc
serg11111 <vardanarsb@gmail.com>
2019-11-26 22:01:49 +0200

    Add Button Script
    Add Button Script

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        transform.localScale = new Vector3(0.02F, 0.02F, 0.02F);
+    }
+
+    private void OnMouseUp()
+    {
+        transform.localScale = new Vector3(0.0187F, 0.0187F, 0.0187F);
+    }
+}
