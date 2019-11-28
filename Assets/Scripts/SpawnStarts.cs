d7fefd9a510af19849f01e72fe7cc2e253de493c
serg11111 <vardanarsb@gmail.com>
2019-11-29 00:17:09 +0200

    Upgrade star sprite
    Upgrade star sprite

--- Assets/Scripts/SpawnStarts.cs
+++ Assets/Scripts/SpawnStarts.cs
@@ -4,24 +4,21 @@ using UnityEngine;
 
 public class SpawnStarts : MonoBehaviour
 {
-
     public GameObject star;
-
     void Start()
     {
         StartCoroutine( spawn());    
     }
-
     IEnumerator spawn()
     {
         while (true)
         {
             //появление звезд будет случайным по высоте и ширине нашего экрана 
-            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), Camera.main.farClipPlane / 2));
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), Camera.main.farClipPlane/10));
             //создаем звезду в позиции pos, без вращения 
             Instantiate(star, pos, Quaternion.identity);
             //каждую секунду создается
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(5.1f);
         }
     }
     
---
+++ Assets/Scripts/StarAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarAnimation : MonoBehaviour
+{
+
+    private SpriteRenderer star;
+    // Start is called before the first frame update
+    void Start()
+    {
+        star = GetComponent<SpriteRenderer>();
+        Destroy(gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        star.color = new Color(star.color.r, star.color.g, star.color.b, Mathf.PingPong(Time.time / 2.5f, 1.0f));
+    }
+}
