e6031633527dd2d2018a5cd917eac9e2ec176af3
serg11111 <vardanarsb@gmail.com>
2019-11-29 13:55:56 +0200

    Update Detect Click/Buttons
    Update Detect Click and Buttons

---
+++ Assets/Scripts/MainScene/DetectClics.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectClics : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        print("Click!");
+    }
+}
