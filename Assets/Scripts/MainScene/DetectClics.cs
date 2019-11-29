5f1770287885e2dd66b0ded16cba21e7e6cfa297
serg11111 <vardanarsb@gmail.com>
2019-11-29 14:34:47 +0200

    Add StartGameCube animation
    Add StartGameCube animation

--- Assets/Scripts/MainScene/DetectClics.cs
+++ Assets/Scripts/MainScene/DetectClics.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class DetectClics : MonoBehaviour
 {
+    public Text gameName, playText;
+    public GameObject buttons, m_cube;
+    private bool clicked;
+
     private void OnMouseDown()
     {
-        print("Click!");
+        if (!clicked)
+        {
+            clicked = true;     //Works only ones 
+            playText.gameObject.SetActive(false);
+            gameName.text = "0";
+            buttons.GetComponent<ScrollObjects>().speed = -10f;
+            buttons.GetComponent<ScrollObjects>().checkPos = -150f;
+            m_cube.GetComponent<Animation>().Play("StartGameCube");
+        }
     }
 }
