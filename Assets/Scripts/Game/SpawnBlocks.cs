8f4943c7a956a01b2bfd3900d60e3b37cd080bf0
serg11111 <vardanarsb@gmail.com>
2019-12-01 00:19:35 +0200

    Update SpawnBlock script
    Add RangeSkale method to SpawnBlock script

--- Assets/Scripts/Game/SpawnBlocks.cs
+++ Assets/Scripts/Game/SpawnBlocks.cs
@@ -11,8 +11,8 @@ public class SpawnBlocks : MonoBehaviour
     void Start()
     {
         blockPos = new Vector3(Random.Range(1.2f, 1.7f), Random.Range(-2f, 2f), 3f);
-        blockInst = Instantiate(block, new Vector3(5f, -5f, 0), Quaternion.identity) as GameObject;
-        blockInst.transform.localScale = new Vector3(Random.Range(1.5f, 2.5f), blockInst.transform.localScale.y, blockInst.transform.localScale.z);
+        blockInst = Instantiate(block, new Vector3(5f, -5f, 0f), Quaternion.identity) as GameObject;
+        blockInst.transform.localScale = new Vector3(RandSkale(), blockInst.transform.localScale.y, blockInst.transform.localScale.z);
     }
 
     private void Update()
@@ -21,5 +21,16 @@ public class SpawnBlocks : MonoBehaviour
         {
             blockInst.transform.position = Vector3.MoveTowards(blockInst.transform.position, blockPos, Time.deltaTime * speed);
         }
+    }
+    float RandSkale()
+    {
+        float rand;
+        if (Random.Range(0, 100) > 80)
+        {
+            rand = Random.Range(1.5f, 2.5f);
+        }
+        else rand = Random.Range(1.5f, 2f);
+
+        return rand;
     }
 }
