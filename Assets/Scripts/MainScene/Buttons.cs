25cd5f98d5839f25ef552c6b133d7e27844bc807
serg11111 <vardanarsb@gmail.com>
2019-11-30 12:12:00 +0200

    Fixed all button
    Fixed all button

--- Assets/Scripts/MainScene/Buttons.cs
+++ Assets/Scripts/MainScene/Buttons.cs
@@ -6,11 +6,11 @@ public class Buttons : MonoBehaviour
 {
     private void OnMouseDown()
     {
-        transform.localScale = new Vector3(0.02F, 0.02F, 0.02F);
+        transform.localScale = new Vector3(1.2F, 1.2F, 1.2F);
     }
 
     private void OnMouseUp()
     {
-        transform.localScale = new Vector3(0.0187F, 0.0187F, 0.0187F);
+        transform.localScale = new Vector3(1F, 1F, 1F);
     }
 }
