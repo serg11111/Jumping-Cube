b5e0d2f8bdc71d05e2555bd0baaf0f9999380415
serg11111 <vardanarsb@gmail.com>
2019-11-30 12:57:59 +0200

    Add CubeToBlock Animation
    Add CubeToBlock Animation

--- Assets/Scripts/MainScene/FallCube.cs
+++ Assets/Scripts/MainScene/FallCube.cs
@@ -13,6 +13,6 @@ public class FallCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, 0.1f, 0);
+        transform.position += new Vector3(0, 0.2f, 0);
     }
 }
--- Assets/Scripts/MainScene/GameArrangement.cs
+++ Assets/Scripts/MainScene/GameArrangement.cs
@@ -8,12 +8,13 @@ public class GameArrangement : MonoBehaviour
     public GameObject[] cubes;
     public Text gameName, playText;
     public GameObject buttons, m_cube;
-    public Animation cube_anim;
+    public Animation cube_anim, block;
     public Light dirLight;
     private bool clicked = false;
 
     private void Update()
     {
+        //уменьшить яркость
         if( clicked && dirLight.intensity != 0)
         {
             dirLight.intensity -= 0.03f;
@@ -30,11 +31,12 @@ public class GameArrangement : MonoBehaviour
             buttons.GetComponent<ScrollObjects>().speed = -10f;
             buttons.GetComponent<ScrollObjects>().checkPos = -150f;
             m_cube.GetComponent<Animation>().Play("StartGameCube");
+            StartCoroutine(cubeToBlock());
             m_cube.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
             cube_anim.GetComponent<Animation>().Play();
         }
     }
-
+    //удаление лишних кубиков
     IEnumerator delCubes()
     {
         for (int i = 0; i < 3; i++)
@@ -42,5 +44,11 @@ public class GameArrangement : MonoBehaviour
             yield return new WaitForSeconds(0.5f);
             cubes[i].GetComponent<FallCube>().enabled = true;
         }
+    }
+    //превращение последнего кубика в блок-платформу
+    IEnumerator cubeToBlock()
+    {
+        yield return new WaitForSeconds(m_cube.GetComponent<Animation>().clip.length + 0.3f );
+        block.Play();
     }
 }
