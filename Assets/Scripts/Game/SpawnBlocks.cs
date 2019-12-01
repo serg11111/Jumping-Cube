e82b09b228c6b94d870cfe203b3d5521bc261883
serg11111 <vardanarsb@gmail.com>
2019-12-01 21:48:45 +0200

    Update SpawnBlocks
    Update SpawnBlocks and AllCube material

--- Assets/Scripts/Game/SpawnBlocks.cs
+++ Assets/Scripts/Game/SpawnBlocks.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SpawnBlocks : MonoBehaviour
 {
-    public GameObject block;
-    public GameObject blockInst;
+    public GameObject block, allCubes;
+    private GameObject blockInst;
     private Vector3 blockPos;
     public float speed = 10f;
+    private bool onPlace;
+
     void Start()
     {
         //куда блок должен прибыть
@@ -16,14 +18,18 @@ public class SpawnBlocks : MonoBehaviour
         blockInst = Instantiate(block, new Vector3(5f, -5f, 0f), Quaternion.identity) as GameObject;
         //формирование размера блока
         blockInst.transform.localScale = new Vector3(RandSkale(), blockInst.transform.localScale.y, blockInst.transform.localScale.z);
+        blockInst.transform.parent = allCubes.transform;
     }
 
     private void Update()
     {
-        if(blockInst.transform.position != blockPos)
+        if(blockInst.transform.position != blockPos && !onPlace)
         {
             //перемещение блока
             blockInst.transform.position = Vector3.MoveTowards(blockInst.transform.position, blockPos, Time.deltaTime * speed);
+        }else if(blockInst.transform.position == blockPos)
+        {
+            onPlace = true;
         }
     }
 
