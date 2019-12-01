c4ac18659b31600f35086574401c0cf1f7d1e102
serg11111 <vardanarsb@gmail.com>
2019-12-02 01:47:05 +0200

    Add Harder and Easier animation
    Add Harder and Easier animation

---
+++ Assets/Scripts/Game/Harder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harder : MonoBehaviour
+{
+    private bool hard;
+    void Update()
+    {
+        if (CubeJump.count_blocks > 0)
+        {
+            if (CubeJump.count_blocks % 2 == 0 && !hard)
+            {
+                print("Harder");
+                hard = true;
+            }
+            else if (CubeJump.count_blocks % 3 == 0 && hard)
+            {
+                hard = false;
+                print("Easier");
+            }
+        }
+    }
+}
