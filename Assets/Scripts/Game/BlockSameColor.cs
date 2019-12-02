0d53e386b62588c75af050b79aa5df4ef3ea6ea1
serg11111 <vardanarsb@gmail.com>
2019-12-03 00:43:51 +0200

    Add BlockSameColor script
    Add BlockSameColor script
    
    If the cube touches the platform (except the first), it paints it in its color

---
+++ Assets/Scripts/Game/BlockSameColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSameColor : MonoBehaviour
+{
+    private bool firstOne;
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        //если куб коснулся платформы(кроме первой) он окрашивает ее в свой цвет
+        if (other.gameObject.tag == "Cube" && firstOne) 
+        {
+            other.gameObject.GetComponent<MeshRenderer>().material.color = GetComponent<MeshRenderer>().material.color;
+        }
+        if (!firstOne)
+        {
+            firstOne = true;
+        }
+    }
+}
